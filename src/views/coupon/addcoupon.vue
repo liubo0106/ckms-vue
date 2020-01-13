cf48dca80ffa22b634fa5cf9de9113ae7d6da9a6
dangxing <1240797544@qq.com>
2020-01-13 11:12:35 +0800

    '优惠卷'

--- src/views/coupon/addcoupon.vue
+++ src/views/coupon/addcoupon.vue
@@ -36,7 +36,7 @@
                                     <el-radio-group v-model="couponData.sendingMethod" @change="checkTime">
                                         <el-radio label="1">每周</el-radio>
                                         <!--                                <el-radio label="2">每月</el-radio>-->
-                                        <!--<el-radio label="3">自定义时间</el-radio>-->
+                                        <el-radio label="3">自定义时间</el-radio>
                                     </el-radio-group>
                                 </el-form-item>
                                 <el-form-item label="选择时间" v-if="showTime">
